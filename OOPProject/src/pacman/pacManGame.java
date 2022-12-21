9406c632953ba009478e39265e69d5ca9afe23e9
nages <nages@LAPTOP-OJOR0724>
2022-12-21 13:08:30 +0530

    pacman

--- OOPProject/src/pacman/pacManGame.java
+++ OOPProject/src/pacman/pacManGame.java
@@ -1,11 +1,19 @@
 package pacman;
 
+import java.util.Scanner;
+
 public class pacManGame {
 
 	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
 		String arr[][] = new String[15][15];
 		
 		int x,y;
+		
+		int n=2, m=2;
+		char move;
+		int score = 0;
 		
 		//STORAGE OF THE MAP
 		
@@ -44,7 +52,19 @@ public class pacManGame {
 					arr[x][y]="#";
 				}
 				
+				else if(y==5 && x>8 && x<14)
+				{
+					arr[x][y]="#";
+				}
+				
+				else if(y==3 && x>4 && x<9)
+				{
+					arr[x][y]="#";
+				}
+				
+				
 				else{
+					arr[12][12]="0";
 					arr[x][y]=".";
 				}
 				
@@ -52,11 +72,88 @@ public class pacManGame {
 		}
 		
 		//PRINTING THE MAP
+		arr[n][m]="@";
+		System.out.println("\tSCORE:" +score);
 		for(x=0;x<15;x++) {
 			for(y=0;y<15;y++) {
 				System.out.print(arr[x][y]+" ");
 			}
 			System.out.println();
+		}
+		
+		while(true) {
+			System.out.print("\nEnter a move: ");
+			move=sc.next().charAt(0);
+			
+			if (move=='s')
+			{
+				n++;
+				if(n>0) {
+					arr[n-1][m]=" ";
+				}
+			}
+			
+			if (move=='w')
+			{
+				n--;
+				if(n>0) {
+					arr[n+1][m]=" ";
+				}
+			}
+			
+			if (move=='a')
+			{
+				m--;
+				if(n>0) {
+					arr[n][m+1]=" ";
+				}
+			}
+			
+			if (move=='d')
+			{
+				m++;
+				if(n>0) {
+					arr[n][m-1]=" ";
+				}
+			}
+			
+			if(arr[n][m]=="0") {
+				
+				for(int i=0; i<20; i++) {
+					System.out.println();
+				}
+				System.out.println("\nFINISH! ");
+				System.out.println("\tSCORE:" +score);
+				break;
+			}
+if(arr[n][m]=="#") {
+				
+				for(int i=0; i<20; i++) {
+					System.out.println();
+				}
+				System.out.println("\nGAME OVER! ");
+				System.out.println("\tSCORE:" +score);
+				
+				break;
+			}
+
+if(arr[n][m]==".") {
+	score+=1;
+}
+
+
+for(int i=0; i<20; i++) {
+	System.out.println();
+}
+
+			arr[n][m]="@";
+			System.out.println("\tSCORE:" +score);
+			for(x=0;x<15;x++) {
+				for(y=0;y<15;y++) {
+					System.out.print(arr[x][y]+" ");
+				}
+				System.out.println();
+			}
 		}
 		
 
